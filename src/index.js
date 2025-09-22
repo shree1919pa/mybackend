ae8a39adba6f6dc06fbf65e3fd8c76608df0cb1d
shree1919pa <202401100024@mitaoe.ac.in>
2025-09-23 00:46:13 +0530

    last changes

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import mongoose, { connect } from "mongoose";
+import {DB_NAME} from "./constant";
+
+import express from "express";
+const app=express();
+(async()=>{
+    try{
+        await mongoose.connect('${process.env.MONGODB_URI} / ${DB_NAME}')
+        app.on("error",()=>{
+            console.log("ERROR : ",error);
+            throw err;
+            
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log('The application is listen by $ {process.env.PORT}')
+        })
+
+    }catch(error){
+        console.error("ERROR : ", error);
+        throw err;
+    }
+
+})()
